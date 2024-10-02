ae3e1f349a1ee89d426b0ba75c77c85e2fd35cf5
Benjamin Funk <lordbaracuta90@gmail.com>
2024-10-02 10:32:11 -0230

    Added the animated background for the splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,25 @@ function App() {
 
   return (
     <>
-      <div className="background-image"></div>
-      <div>
-        <img className="logo" src="src/assets/Emblem 1 3.png" alt="" />
+      <div className="background-image">
+        <img src="src/assets/Anûm_ The Continent of Isceria v1.1.png" alt="" />
+        <img src="src/assets/0x8vuwfue9391.webp" alt="" />
+        <img src="src/assets/Worldmap.jpg" alt="" />
       </div>
-      <h1>Campaign Companion</h1>
-      <div className="card">
-        <button>New Campaign</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="splash-items">
+        <img className="logo" src="src/assets/Emblem 1 3.png" alt="" />
+        <h1>Campaign Companion</h1>
+        <div className="card">
+          <button>New Campaign</button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+         More to come later
         </p>
       </div>
-      <p className="read-the-docs">
-        More to come later
-      </p>
+      
     </>
   )
 }
