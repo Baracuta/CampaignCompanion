56b3cf56b375e1a7ee624c30376b83dae552118c
Benjamin Funk <lordbaracuta90@gmail.com>
2024-10-04 10:40:01 -0230

    oh my god some serious amount of suffering happening 'ere b'ys

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { Navigate, Router, useNavigate } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import{
+  createBrowserRouter, RouterProvider,} from "react-router-dom";
+
+const router=createBrowserRouter([{
+  path: "/campaignTest", element: <div>This is the test place!"</div>
+},
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
