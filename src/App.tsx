ddebfc993ccdd3981909d5bd69de6ff846ec4f10
Benjamin Funk <lordbaracuta90@gmail.com>
2024-10-09 12:07:04 -0230

    Un-screwed a bunch of stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import './App.css'
-import{Link, BrowserRouter as Router,} from "react-router-dom";
+import{Link, Navigate, BrowserRouter as Router,} from "react-router-dom";
 
 
 function App() {
   return (
     <>
+
       <div className="background-image">
+
         <img src="src/assets/Anûm_ The Continent of Isceria v1.1.png" alt="" />
         <img src="src/assets/0x8vuwfue9391.webp" alt="" />
         <img src="src/assets/Worldmap.jpg" alt="" />
+        
       </div>
+
       <div className="splash-items">
+
         <img className="logo" src="src/assets/Emblem 1 3.png" alt="" />
+
         <h1>Campaign Companion</h1>
+
         <div className="card">
           <button>New Campaign</button>
         </div>
-        <Router>
-          <div>
-            <Link to="/pagetest"/>
-          </div>
-        </Router>
+
       </div> 
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,8 @@ import App from './App.tsx'
 import './index.css'
 import{createBrowserRouter, RouterProvider, BrowserRouter} from "react-router-dom";
 
-const testRouter=createBrowserRouter([{
-  path: "/pagetest",
-}
-])
+
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
     <App />
-    <RouterProvider router={testRouter}/>
-  </BrowserRouter>,
 )
