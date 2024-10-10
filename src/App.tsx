452d751602209c258c9bd90c73e8d0a3170d9758
Benjamin Funk <lordbaracuta90@gmail.com>
2024-10-10 17:43:53 -0230

    pain as I attempt to put the rotating splashScreen background in its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-
+import splashBackground from './components/SplashBackground'
 
 function App() {
   return (
---
+++ src/components/SplashBackground.tsx
@@ -0,0 +1,15 @@
+
+
+function splashBackground(){
+    return(
+        <div className="background-image">
+
+         <img src="src/assets/Anûm_ The Continent of Isceria v1.1.png" alt="" />
+         <img src="src/assets/0x8vuwfue9391.webp" alt="" />
+         <img src="src/assets/Worldmap.jpg" alt="" />
+
+        </div>
+    )
+}
+
+export default splashBackground
