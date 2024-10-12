4900f211dd272ea80112367b0c6571ebd391ba71
Benjamin Funk <lordbaracuta90@gmail.com>
2024-10-12 13:50:31 -0230

    Changed the name pf pagetest to TestingPage;

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import TestPage from './Pages/pagetest.tsx'
+import TestPage from './Pages/TestingPage.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 
