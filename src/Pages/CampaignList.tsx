dd43bc4322c231b91a10fad59ce05fd92b3a2308
Benjamin Funk <lordbaracuta90@gmail.com>
2024-10-14 09:39:17 -0230

    Added comment to CampaignList

--- src/Pages/CampaignList.tsx
+++ src/Pages/CampaignList.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom"
 import HomeButton from "../components/HomeButton"
 
-
+//This page will list out all of the user's created campaigns
 function CampaignList(){
     const navigate=useNavigate();
     return(
