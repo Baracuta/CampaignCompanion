09b05b4bdb9b978b8753f4950b33653fc675f79d
Benjamin Funk <lordbaracuta90@gmail.com>
2024-10-29 13:39:39 -0230

    More work setting up CampaignForm

--- src/components/Campaign.tsx
+++ src/components/Campaign.tsx
@@ -1,22 +1,24 @@
 import { useParams } from "react-router-dom";
 
-function Campaign(){
-    const params=useParams();
-
-    const id1=params.id1;
-    const id2=params.id2;
-    const id3=params.id3;
-
-    return((
-        <div>
-            <h1>(id1) / (id2) / (id3)</h1>
-        </div>
-    ))
-}
-
-export default Campaign
-
 export type Campaign={
     id:number;
     name:string;
+}
+
+
+export const getCampaigns = async (): Promise<Campaign[]> => {
+    return[
+        {
+            id:1,
+            name: 'Campaign 1',
+        },
+        {
+            id:2,
+            name: 'Campaign 2',
+        },
+        {
+            id:3,
+            name: 'Campaign 3',
+        }
+    ]
 }
