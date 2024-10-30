73a18c3fc331d503cd6d7db0bf058fbd408f693a
Benjamin Funk <lordbaracuta90@gmail.com>
2024-10-30 16:38:59 -0230

    Enhancing the readability of some of my code

--- src/Pages/CampaignPage.tsx
+++ src/Pages/CampaignPage.tsx
@@ -26,20 +26,33 @@ function CampaignPage(){
             <div className={styles.top_bar}>
 
                 <img src="src\assets\Emblem 1 3.png"/>
+
                 <h1>{campaign.name}</h1>
+
                 <NavButton text="Go Back" destination="/campaign-form"/>
 
             </div>
+
             <div className={styles.recently_editted}>Recently Editted</div>
+
             <div className={styles.content_bulk}>
+
                 <div className={styles.side_bar}>SideBar</div>
+
                 <div className={styles.card_panel}>
+
                     <div className={styles.card}>Non-Player Characters</div>
+
                     <div className={styles.card}>Locations</div>
+
                     <div className={styles.card}>Items</div>
+
                 </div>
+
                 <div className={styles.calendar}>Calendar</div>
+
             </div>
+            
             <div className={styles.bottom_bar}>Bottombar</div>
 
         </main>
