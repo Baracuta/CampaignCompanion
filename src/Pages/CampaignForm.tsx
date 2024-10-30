7f7c078f29a621e6231c3c13a28ce26266e18941
Benjamin Funk <lordbaracuta90@gmail.com>
2024-10-30 11:48:13 -0230

    More work work work on the CampaignForm

--- src/Pages/CampaignForm.tsx
+++ src/Pages/CampaignForm.tsx
@@ -13,10 +13,18 @@ function CampaignForm(){
             </div>
 
             <div className={styles.form_body}>
-
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
+                <div id="input1">
+                    <p>What game system does this campaign use?</p>
+                    <input type="text" />
+                </div>
+                <div id="input2">
+                    <p>What would you like to name this campaign?</p>
+                    <input type="text" />
+                </div>
+                <div id={styles.input3}>
+                    <p>Please write a short description</p>
+                    <input type="text" />
+                </div>
             </div>
 
 
