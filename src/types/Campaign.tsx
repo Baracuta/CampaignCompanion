bd388aa8db8d6c339fdc66bcd76a75265a8ffca6
Benjamin Funk <lordbaracuta90@gmail.com>
2024-11-04 12:23:38 -0330

    Minor fixes in Campaign.tsx

--- src/types/Campaign.tsx
+++ src/types/Campaign.tsx
@@ -11,15 +11,15 @@ export type Campaign={
 export const getCampaigns = async (): Promise<Campaign[]> => {
     return[
         {
-            id:1,
+            id:"1",
             name: 'Campaign 1',
         },
         {
-            id:2,
+            id:"2",
             name: 'Campaign 2',
         },
         {
-            id:3,
+            id:"3",
             name: 'Campaign 3',
         }
     ]
