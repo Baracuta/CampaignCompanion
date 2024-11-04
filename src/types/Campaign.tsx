90f9f75c78447b72be9810e21652791cecc717a1
Benjamin Funk <lordbaracuta90@gmail.com>
2024-11-04 13:42:53 -0330

    Made Campaign Type ONLY the type Campaign

--- src/types/Campaign.tsx
+++ src/types/Campaign.tsx
@@ -5,21 +5,3 @@ export type Campaign={
     game?:string;
     players?:number;
 }
-
-
-export const getCampaigns = async (): Promise<Campaign[]> => {
-    return[
-        {
-            id:"1",
-            name: 'Campaign 1',
-        },
-        {
-            id:"2",
-            name: 'Campaign 2',
-        },
-        {
-            id:"3",
-            name: 'Campaign 3',
-        }
-    ]
-}
