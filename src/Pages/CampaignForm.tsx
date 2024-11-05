88a302f0c6b54a22a0fd00de908e37e9cfb820ed
Benjamin Funk <lordbaracuta90@gmail.com>
2024-11-05 12:07:08 -0330

    I'M BEGINNING TO BELIEVE

--- src/Pages/CampaignForm.tsx
+++ src/Pages/CampaignForm.tsx
@@ -6,17 +6,6 @@ import { Campaign } from "../types/Campaign";
 //This page is for the campaign creation form
 function CampaignForm(){
     const [campaign, setCampaign]=useState<Partial<Campaign>>({});
-
-    const [id, setId]=useState("0");
-    const [name, setName]=useState("Test Campaign");
-    const [game, setGame]=useState("Pathfinder");
-    const [players, setPlayers]=useState("0");
-
-    const campaignTest: Campaign={
-        id:(id),
-        name:(name),
-        game:(game)
-    }
     
 
 
@@ -30,32 +19,50 @@ function CampaignForm(){
             <div className={styles.form_body}>
 
                 <div id={styles.section1}>
+
                     <p>What game system does this campaign use?</p>
 
                     <input
                     type="text"
-                    value={game}
-                    onChange={e => setGame(e.target.value)}
+                    value={campaign.game}
+                    onChange={e => {
+                        const game=e.target.value;
+                        setCampaign({...campaign, game});
+                    }}
                     />
 
                 </div>
 
                 <div id={styles.section2}>
+
                     <p>What would you like to name this campaign?</p>
+
                     <input
                     type="text"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
+                    value={campaign.name}
+                    onChange={e => {
+                        const name=e.target.value;
+                        setCampaign({...campaign, name});
+                    }}
                     />
-                    <p>{name}</p>
+
+                    <p>{campaign.name}</p>
+
                 </div>
 
                 <div id={styles.section3}>
+
                     <p>How many players are in this campaign?</p>
+
                     <input
                     type="text"
-                    value={players}
-                    onChange={e => setPlayers(e.target.value)}/>
+                    value={campaign.players}
+                    onChange={e => {
+                        const players=e.target.value;
+                        setCampaign({...campaign, players});
+                    }}
+                    />
+
                 </div>
 
                 <button onClick={console.log}>Create Campaign</button>
