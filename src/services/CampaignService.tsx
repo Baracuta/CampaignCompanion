0c42105b4e0c4a1af54e7073910d4af6bcbaf36a
Benjamin Funk <lordbaracuta90@gmail.com>
2024-11-11 20:35:55 -0330

    Setting up localStorage stuff in CampaignService

---
+++ src/services/CampaignService.tsx
@@ -0,0 +1,15 @@
+import { Campaign } from "../types/Campaign";
+
+export const createCampaign = async (campaign: Campaign): Promise<Campaign> => {
+    const allCampaignsString = localStorage.getItem("campaigns");
+    const allCampaigns = allCampaignsString == null ? [] : JSON.parse(allCampaignsString);
+
+    const newCampaigns = [
+        ...allCampaigns,
+        campaign
+    ];
+
+    localStorage.setItem("campaigns", JSON.stringify(newCampaigns));
+
+    return campaign;
+}
