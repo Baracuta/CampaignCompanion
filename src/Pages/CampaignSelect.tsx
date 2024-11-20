6284daa04a48bfdc7d2394de1666777f857a0292
Benjamin Funk <lordbaracuta90@gmail.com>
2024-11-20 11:11:29 -0330

    Getting ready to try a few things to get the CampaignSelect screen to actually make a card for each campaign.

--- src/Pages/CampaignSelect.tsx
+++ src/Pages/CampaignSelect.tsx
@@ -1,22 +1,32 @@
 import styles from '../css_modules/select.module.css'
 import NavButton from "../components/NavButton"
+import Card from '../components/Card'
 import { getCampaigns } from "../services/CampaignService"
 
 
 
 function CampaignSelect(){
+    let n=getCampaigns
 
     return(
 
         <main className={styles.main}>
 
-            <div>
+            <div className={styles.top_bar}>
                 Campaign Selection Screen
+
+                <NavButton text="Return to Splash Screen" destination="/"/>
+
+                <button onClick={getCampaigns}>Get Campaigns</button>
+
+                
+
             </div>
 
-            <NavButton text="Return to Splash Screen" destination="/"/>
-
-            <button onClick={getCampaigns}>Get Campaigns</button>
+            <div className={styles.card_panel}>
+                <Card className={styles.card} name="Campaign 1" cardLink='/campaign/:id'/>
+            </div>
+            
 
         </main>
 
