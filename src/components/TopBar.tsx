4689bd2ab9363e8ddc68052430e400a82f2ad8ae
Benjamin Funk <lordbaracuta90@gmail.com>
2024-11-26 11:03:13 -0330

    TopBarProps and some topbar css

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,12 @@
 import styles from '../css_modules/topbar.module.css'
 
-function TopBar(){
+type TopBarProps={
+    campaignName?:string;
+    campaignGame?:string;
+    image?:string;
+}
+
+function TopBar(props:TopBarProps){
 
     return(
         <div className={styles.top_bar}>
@@ -8,8 +14,8 @@ function TopBar(){
                 <img src={`${ASSETS_PATH}/Emblem 1 3.png`}/>
 
                 <div className={styles.title_box}>
-                    <h2>{campaign?.name}</h2>
-                    <h3>{campaign?.game}</h3>
+                    <h2>{props?.campaignName}</h2>
+                    <h3>{props?.campaignGame}</h3>
                 </div>
 
                 <div className={styles.dashboard_menu}>
