987d622ae26b79e1c28d49e8a5a675e7a14513bf
Benjamin Funk <lordbaracuta90@gmail.com>
2024-12-02 10:34:41 -0330

    I take back what I said, this is getting way out of hand.

--- src/components/NPCList.tsx
+++ src/components/NPCList.tsx
@@ -1,12 +1,25 @@
 import Card from "./Card";
-import { getNPCs } from "../services/CampaignService";
+import { getCampaign, getNPCs } from "../services/CampaignService";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Campaign } from "../types/Campaign";
 
 
 function NPCList(){
-    const list=getNPCs();
+    const {id}=useParams();
+
+    const [campaign, setCampaign] = useState<Campaign>();
+
+    useEffect(()=>{
+        getCampaign (id as string).then((campaign)=>{
+            setCampaign(campaign);
+        })
+    }, [id]);
+
+    const list=getNPCs(id as string);
 
     const divs=list.map((datum)=>
-        
+
     <Card
     name={datum.name}
     cardLink={``}>
