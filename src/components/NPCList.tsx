771ba088d1b18368d493a0cd6296122fe0552427
Benjamin Funk <lordbaracuta90@gmail.com>
2024-12-02 10:38:11 -0330

    NPCList is back to how it was.

--- src/components/NPCList.tsx
+++ src/components/NPCList.tsx
@@ -1,22 +1,9 @@
 import Card from "./Card";
-import { getCampaign, getNPCs } from "../services/CampaignService";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { Campaign } from "../types/Campaign";
+import { getNPCs } from "../services/CampaignService";
 
 
 function NPCList(){
-    const {id}=useParams();
-
-    const [campaign, setCampaign] = useState<Campaign>();
-
-    useEffect(()=>{
-        getCampaign (id as string).then((campaign)=>{
-            setCampaign(campaign);
-        })
-    }, [id]);
-
-    const list=getNPCs(id as string);
+    const list=getNPCs();
 
     const divs=list.map((datum)=>
 
