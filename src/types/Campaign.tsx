93c2acf35144697cb60b2aa15777cbab876adf03
Benjamin Funk <lordbaracuta90@gmail.com>
2024-12-03 12:36:05 -0330

    One final bloody comment in campaign type

--- src/types/Campaign.tsx
+++ src/types/Campaign.tsx
@@ -3,7 +3,7 @@ import { Location } from "./Location";
 import { Item } from "./Item";
 import { PlayerCharacter } from "./PlayerCharacter";
 
-
+//For godsake, if there's a way to tell this thing "if undefined, actually just return an empty set" that'd be grand
 export type Campaign={
     id:string;
     name?:string;
