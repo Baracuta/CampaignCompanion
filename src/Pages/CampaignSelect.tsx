86f9d7f0600f1fff0e53af827e25b9baa6ca27d7
Benjamin Funk <lordbaracuta90@gmail.com>
2024-12-08 15:10:16 -0330

    Prettiered CampaignSelect. Also going to be a slow day since I was up late last night designated driving.

--- src/Pages/CampaignSelect.tsx
+++ src/Pages/CampaignSelect.tsx
@@ -1,27 +1,23 @@
-import styles from '../css_modules/select.module.css'
-import { ASSETS_PATH } from '../constants/assets_path'
-import CampaignList from '../components/CampaignList'
-import CardPanel from '../components/CardPanel'
-import TopBar from '../components/TopBar'
+import styles from "../css_modules/select.module.css";
+import { ASSETS_PATH } from "../constants/assets_path";
+import CampaignList from "../components/CampaignList";
+import CardPanel from "../components/CardPanel";
+import TopBar from "../components/TopBar";
 
+function CampaignSelect() {
+  return (
+    <main className={styles.main}>
+      <TopBar
+        name="Campaign Selection"
+        buttonNav="/"
+        image={`${ASSETS_PATH}/Emblem 1 3.png`}
+      />
 
-
-function CampaignSelect(){
-
-    return(
-
-        <main className={styles.main}>
-
-            <TopBar name='Campaign Selection' buttonNav='/' image={`${ASSETS_PATH}/Emblem 1 3.png`}/>
-
-            <CardPanel>
-                <CampaignList/>
-            </CardPanel>
-            
-
-        </main>
-
-    )
+      <CardPanel>
+        <CampaignList />
+      </CardPanel>
+    </main>
+  );
 }
 
-export default CampaignSelect
+export default CampaignSelect;
