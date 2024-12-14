1b97126d5473427300df93cdc8ad326f22f8c1e6
Benjamin Funk <lordbaracuta90@gmail.com>
2024-12-14 16:23:20 -0330

    Setting up the display thing to accept multiple displayTypes, even though realistically I won't need this functionality unless I'm going to have just the one component. Better to have it and not need it than need it and not have it.

--- src/components/NPCDisplay.tsx
+++ src/components/NPCDisplay.tsx
@@ -1,8 +1,11 @@
+import styles from '../css_modules/display.module.css'
+
 //This component is going to be the actual pop-open window that appears on-screen when an entity card is clicked.
 //If you click on an NPC, it'll present the npc stuff. If you click on a location, it'll present the location stuff.
 //Going to make this one strictly for NPCs for the time being, and if it turns out that there's a way to make one component function to provide for all 4 entity types, then we'll do that.
 
 type DisplayProps={
+    displayType:'npc'|'location'|'item'|'playerCharacter';
     name?:string;
     description?:string;
     notes?:string;
@@ -10,5 +13,19 @@ type DisplayProps={
 }
 
 function NPCDisplay(props:DisplayProps){
-    
+
+    let divClass=(styles.display)
+    if (props.displayType !=null){
+        divClass=`${divClass} ${styles[props.displayType]}`;
+    }
+
+
+    return(
+        <div className={divClass}>
+
+        </div>
+    )
 }
+
+
+export default NPCDisplay
