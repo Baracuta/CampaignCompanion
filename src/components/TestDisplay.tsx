eec1a78fb69670ac4617215dd569780d8b7aff9e
Benjamin Funk <lordbaracuta90@gmail.com>
2024-12-15 16:51:10 -0330

    Props added to TestDisplay, going to try it now.

--- src/components/TestDisplay.tsx
+++ src/components/TestDisplay.tsx
@@ -5,25 +5,30 @@ import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
+import styles from '../css_modules/card.module.css';
 
-export default function TestDisplay() {
+type DisplayProps = {
+    name?: string;
+    description?: string;
+    notes?: string;
+    image?: string;
+  };
+
+export default function TestDisplay(props:DisplayProps) {
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
-
   const handleClose = () => {
     setAnchorEl(null);
   };
-
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
 
   return (
     <div>
-      <Button aria-describedby={id} variant="contained" onClick={handleClick}>
-        Open Popover
+      <Button className={styles.card} aria-describedby={id} variant="contained" onClick={handleClick}>
+        <p>{props.name}</p>
       </Button>
 
       <Popover
@@ -36,8 +41,12 @@ export default function TestDisplay() {
           horizontal: 'left',
         }}
       >
-        <p>Heyayaya</p>
         <button onClick={handleClose}/>
+        <h1>{props.name}</h1>
+        <h3>{props.description}</h3>
+
+        <textarea/>
+
         <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
 
       </Popover>
