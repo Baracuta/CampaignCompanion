8e5c860958b9e77b28192e583ad3139b0856dbf9
Benjamin Funk <lordbaracuta90@gmail.com>
2025-01-07 11:28:49 -0330

    Deleted the unused cardService

--- src/services/CardService.tsx
+++
@@ -1,2 +0,0 @@
-//I imagine that for each campaign, it will need to be able to compile the cards and their respective data and link them appropriately.
-//Basically, in the same way CampaignService fetches the campaigns, the CardService will look at a campaign's ID and match it to a corresponding set of Cards.
