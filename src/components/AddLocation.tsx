9350700e4e4f83c9032784269f69e829b0e06b4a
Benjamin Funk <lordbaracuta90@gmail.com>
2025-01-07 09:49:40 -0330

    Prettiered and hoisted state in AddLocation

--- src/components/AddLocation.tsx
+++ src/components/AddLocation.tsx
@@ -1,84 +1,82 @@
 import {
-    Button,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-  } from "@mui/material";
-  import styles from "../css_modules/card.module.css";
-  import { Fragment, useState } from "react";
-  import { createLocation } from "../services/CampaignService";
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import styles from "../css_modules/card.module.css";
+import { Fragment, useState } from "react";
 import { Location } from "../types/Location";
 
-  type thingProps = {
-    campaignId: string;
+type thingProps = {
+  campaignId: string;
+  addThing: (location: Location, id: string) => Promise<Location>;
+};
+
+//Set it up to have a similar style to a card, but with unique properties
+function AddLocation(props: thingProps) {
+  const [location, setLocation] = useState<Partial<Location>>({});
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
   };
-  
-  //Set it up to have a similar style to a card, but with unique properties
-  function AddLocation(props: thingProps) {
-    const [location, setLocation] = useState<Partial<Location>>({});
-  
-    const [open, setOpen] = useState(false);
-  
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
-  
-    return (
-      <Fragment>
-        <Button
-          className={styles.card}
-          variant="outlined"
-          onClick={handleClickOpen}
-        >
-          Add Location
-        </Button>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Add a New Location to this Campaign</DialogTitle>
-          <DialogContent>
-            <div className={styles.add_npc}>
-              <h3> Name this Location</h3>
-  
-              <input
-                type="text"
-                value={location.name ?? ""}
-                onChange={(e) => {
-                  const name = e.target.value;
-                  setLocation({ ...location, name });
-                }}
-              />
-  
-              <h3> Describe this Location</h3>
-  
-              <textarea
-                value={location.description ?? ""}
-                onChange={(e) => {
-                  const description = e.target.value;
-                  setLocation({ ...location, description });
-                }}
-              />
-            </div>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button
-              onClick={async () => {
-                await createLocation(location as Location, props.campaignId);
-                handleClose();
-                refreshCampaign();
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Fragment>
+      <Button
+        className={styles.card}
+        variant="outlined"
+        onClick={handleClickOpen}
+      >
+        Add Location
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add a New Location to this Campaign</DialogTitle>
+        <DialogContent>
+          <div className={styles.add_npc}>
+            <h3> Name this Location</h3>
+
+            <input
+              type="text"
+              value={location.name ?? ""}
+              onChange={(e) => {
+                const name = e.target.value;
+                setLocation({ ...location, name });
               }}
-            >
-              Add Location
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </Fragment>
-    );
-  }
-  
-  export default AddLocation;
-  
+            />
+
+            <h3> Describe this Location</h3>
+
+            <textarea
+              value={location.description ?? ""}
+              onChange={(e) => {
+                const description = e.target.value;
+                setLocation({ ...location, description });
+              }}
+            />
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button
+            onClick={() => {
+              props.addThing(location as Location, props.campaignId);
+              handleClose();
+            }}
+          >
+            Add Location
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Fragment>
+  );
+}
+
+export default AddLocation;
