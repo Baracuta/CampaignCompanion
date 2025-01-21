c2fe28446a6e6c8f5c92b521f262d04920b015ec
Benjamin Funk <lordbaracuta90@gmail.com>
2025-01-21 11:45:14 -0330

    Mkay, yeah, starting to be working nicely

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,23 +1,55 @@
-import { ImageList, ImageListItem } from "@mui/material";
+import { Button, ImageList, ImageListItem, Popover } from "@mui/material";
 import { NpcImageData } from "../constants/npc_image_bank";
+import { useState } from "react";
 
-const itemData=NpcImageData;
-
-
+const itemData = NpcImageData;
 
 export default function StandardImageList() {
-    return (
-      <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img}>
-            <img
-              srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-              src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-              alt={item.title}
-              loading="lazy"
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
-    );
-  }
+
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+    setAnchorEl(null);
+    };
+    const open = Boolean(anchorEl);
+    const id = open ? "simple-popover" : undefined;
+
+
+  return (
+    <div>
+      <Button aria-describedby={id} variant="contained" onClick={handleClick}>
+        <p>Button</p>
+      </Button>
+
+      <Popover
+        className="testing"
+        anchorReference="anchorPosition"
+        anchorPosition={{ top: 0, left: 0 }}
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+        <button onClick={handleClose}>Close</button>
+      </Popover>
+    </div>
+  );
+}
