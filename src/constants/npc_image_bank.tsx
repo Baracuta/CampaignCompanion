c52d7e6c125a997bd2badfbc3dcca230356dba5f
Benjamin Funk <lordbaracuta90@gmail.com>
2025-01-22 15:30:00 -0330

    Added remainder of test images (for now)

--- src/constants/npc_image_bank.tsx
+++ src/constants/npc_image_bank.tsx
@@ -21,4 +21,36 @@ export const NpcImageData = [
     img: 'src/assets/npcs/Dwarf m 1.jpg',
     title: 'Dwarf m 1',
   },
+  {
+    img: 'src/assets/npcs/Elf f 1.jpg',
+    title: 'Elf f 1',
+  },
+  {
+    img: 'src/assets/npcs/Elf m 1.jpg',
+    title: 'Elf m 1',
+  },
+  {
+    img: 'src/assets/npcs/Gnome f 1.jpg',
+    title: 'Gnome f 1',
+  },
+  {
+    img: 'src/assets/npcs/Gnome m 1.jpg',
+    title: 'Gnome m 1',
+  },
+  {
+    img: 'src/assets/npcs/Human f 1.jpg',
+    title: 'Human f 1',
+  },
+  {
+    img: 'src/assets/npcs/Human m 1.jpg',
+    title: 'Human m 1',
+  },
+  {
+    img: 'src/assets/npcs/Half-Orc m 1.jpg',
+    title: 'Half-Orc m 1',
+  },
+  {
+    img: 'src/assets/npcs/Tiefling m 1.jpg',
+    title: 'Tiefling m 1',
+  },
 ]
