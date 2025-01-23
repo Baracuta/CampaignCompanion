6aa8e4cd268955fa97950c07596a93ea56edb1fc
Benjamin Funk <lordbaracuta90@gmail.com>
2025-01-23 10:23:41 -0330

    Fixing up some of the stuff for other Details that I broke

--- src/components/DetailsItem.tsx
+++ src/components/DetailsItem.tsx
@@ -13,11 +13,12 @@ function ItemDetails (props:ItemProps){
             <div className={styles.top}>
                 <h2>{props.item.name}</h2>
                 <h3>{props.item.description}</h3>
-            </div>
-            <div className={styles.bottom}>
                 <h5>Category:{props.item.category}</h5>
                 <h5>Effect:{props.item.effect}</h5>
                 <pre className={styles.notes}>{props.item.notes}</pre>
+            </div>
+            <div className={styles.bottom}>
+                
             </div>
         </div>
     )
--- src/components/DetailsPlayerCharacter.tsx
+++ src/components/DetailsPlayerCharacter.tsx
@@ -13,12 +13,13 @@ function PlayerCharacterDetails (props:PCProps){
             <div className={styles.top}>
                 <h2>{props.pc.name}</h2>
                 <h3>{props.pc.description}</h3>
-            </div>
-            <div className={styles.bottom}>
                 <h5>Player Name:{props.pc.playerName}</h5>
                 <h5>Class:{props.pc.pcClass}</h5>
                 <h5>Level:{props.pc.level}</h5>
                 <pre className={styles.notes}>{props.pc.notes}</pre>
+            </div>
+            <div className={styles.bottom}>
+                
             </div>
         </div>
     )
