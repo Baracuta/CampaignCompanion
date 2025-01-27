54b0654e89a04dc77f810e6191db7244d5181b46
Benjamin Funk <lordbaracuta90@gmail.com>
2025-01-27 10:40:03 -0330

    Minor stuff and a comment

--- src/components/AvatarComponent.tsx
+++ src/components/AvatarComponent.tsx
@@ -1,10 +1,13 @@
 import { Avatar } from "@mui/material";
 import { ASSETS_PATH } from "../constants/assets_path";
 
+type AvatarProps={
+
+}
 
 
 
-function AvatarComponent(){
+function AvatarComponent(props:AvatarProps){
 
 
     return(
@@ -16,3 +19,5 @@ function AvatarComponent(){
 }
 
 export default AvatarComponent
+
+//When I want to make this functional, I can use ThingList and pass new campaign props for favourites and for recent edits.
