ca460c05c23bd3db0b79583060aaf8ecd0042d7b
Ben <lordbaracuta90@gmail.com>
2025-03-03 14:01:45 -0330

    Started on NavigateToEntity

---
+++ src/components/NavigateToEntity.tsx
@@ -0,0 +1,12 @@
+import { Entity } from "../types/Entity"
+// This will be used in the AvatarLists and SearchBar
+type NavProps={
+    entity:Entity;
+}
+
+function NavigateToEntity(props:NavProps){
+
+}
+
+
+export default NavigateToEntity
