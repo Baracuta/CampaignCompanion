26ed5e11f36a0e289d4e4d3b384436c912e6d9cf
Ben <lordbaracuta90@gmail.com>
2025-03-04 22:03:03 -0330

    Just a little comment, but I am back

--- src/components/NavigateToEntity.tsx
+++ src/components/NavigateToEntity.tsx
@@ -1,5 +1,6 @@
 import { Entity } from "../types/Entity"
 // This will be used in the AvatarLists and SearchBar
+// Set it up so that it opens the displaything with the passed entity. Should be relatively straightforward.
 type NavProps={
     entity:Entity;
 }
