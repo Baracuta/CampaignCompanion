6e87944ecaa7336def68c0acdead072fe495436d
Benjamin Funk <lordbaracuta90@gmail.com>
2025-03-26 11:27:16 -0230

    Altering base and basename too

--- src/main.tsx
+++ src/main.tsx
@@ -49,7 +49,7 @@ const router=createBrowserRouter([
         path:"/campaign/:id/PlayerCharacters",
         element:<CampaignPlayerCharacters/>
     },
-], {basename: "/Campaign-Companion"})
+], {basename: "/CampaignCompanion"})
 
 createRoot(document.getElementById('root')!).render(
     <RouterProvider router={router}/>
