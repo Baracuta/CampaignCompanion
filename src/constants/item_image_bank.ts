032e381e0dc5c1ad44679a2cbc2d6ee4eff8ac61
Benjamin Funk <lordbaracuta90@gmail.com>
2025-04-08 08:40:26 -0230

    Added images to the item image bank

--- src/constants/item_image_bank.ts
+++ src/constants/item_image_bank.ts
@@ -1,11 +1,51 @@
 
 export const ItemImageData = [
     {
+      img: '/items/amulet1.jpg',
+      title: 'Amulet 1',
+    },
+    {
+      img: '/items/amulet2.jpg',
+      title: 'Amulet 2',
+    },
+    {
+      img: '/items/book.jpg',
+      title: 'Book',
+    },
+    {
+      img: '/items/bow.jpg',
+      title: 'Bow 1',
+    },
+    {
+      img: '/items/crystal.jpg',
+      title: 'Crystal 1',
+    },
+    {
       img: '/items/Sword 1.jpg',
       title: 'Sword 1',
     },
     {
       img: '/items/Dagger 1.jpg',
       title: 'Dagger 1',
+    },
+    {
+      img: '/items/dagger2.jpg',
+      title: 'Dagger 2',
+    },
+    {
+      img: '/items/orb.jpg',
+      title: 'Orb 1',
+    },
+    {
+      img: '/items/staff.jpg',
+      title: 'Staff 1',
+    },
+    {
+      img: '/items/stones.jpg',
+      title: 'Stones 1',
+    },
+    {
+      img: '/items/scroll.jpg',
+      title: 'Scroll 1',
     },
   ]
