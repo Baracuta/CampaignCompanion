d4bbf0522693bf02610db06ee425977cf4c7a348
Benjamin Funk <lordbaracuta90@gmail.com>
2025-07-09 11:52:07 -0230

    Made entity test file and imports, getting to everything outlined in previous commit tomorrow

---
+++ test/controllers/entity.test.ts
@@ -0,0 +1,6 @@
+import { any } from "joi";
+import * as EntityController from "../../src/controllers/EntityController";
+import { Request, Response } from "express";
+import * as uuid from "uuid";
+
+// Self-explanatory, going to be where my tests for the entity controller go.
