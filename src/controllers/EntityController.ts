0a95b5c53e4343b48b4a2cf12212c23ee28f8cd3
Benjamin Funk <lordbaracuta90@gmail.com>
2025-07-15 09:30:51 -0230

    Requesthandlers for create methods written into EntityController.

--- src/controllers/EntityController.ts
+++ src/controllers/EntityController.ts
@@ -2,9 +2,9 @@ import type { RequestHandler } from "express";
 import * as CampaignService from "../services/CampaignServiceConnect";
 import Joi from "joi";
 import { NPC } from "../types/NPC";
-// import { Location } from "../types/Location";
-// import { Item } from "../types/Item";
-// import { PC } from "../types/PlayerCharacter";
+import { Location } from "../types/Location";
+import { Item } from "../types/Item";
+import { PC } from "../types/PlayerCharacter";
 
 // Basically, all the logic that handles the different entity types
 // is going to be in this controller.
@@ -21,17 +21,17 @@ const NPCSchema = {
 };
 
 export const createNPC: RequestHandler = async (req, res): Promise<void> => {
-    const { error, value } = Joi.object(NPCSchema).validate(req.body);
-    if (error) {
-        res.status(400).json({ error: "Invalid NPC data" });
-        return;
-    }
-    // The campaign ID is hardcoded for now
-    const campaign = await CampaignService.getCampaign("1c932bb3-1bae-44aa-8536-c0aadf432882");
+  const { error, value } = Joi.object(NPCSchema).validate(req.body);
+  if (error) {
+      res.status(400).json({ error: "Invalid NPC data" });
+      return;
+  }
+  // The campaign ID is hardcoded for now
+  const campaign = await CampaignService.getCampaign("1c932bb3-1bae-44aa-8536-c0aadf432882");
 
-    await CampaignService.createNPC(campaign.id, value as NPC);
+  await CampaignService.createNPC(campaign.id, value as NPC);
 
-    res.status(201).json("NPC created successfully");
+  res.status(201).json("NPC created successfully");
 };
 
 // export const getNPC: RequestHandler = async (req, res): Promise<void> => {
@@ -48,18 +48,28 @@ export const createNPC: RequestHandler = async (req, res): Promise<void> => {
 
 
 
-// // Location Section
+// Location Section
 
-// const LocationSchema = {
-//   id: Joi.string().uuid().optional(),
-//   name: Joi.string().required(),
-//   description: Joi.string().optional(),
-//   campaignId: Joi.string().uuid().required(),
-// };
+const LocationSchema = {
+id: Joi.string().uuid().optional(),
+name: Joi.string().required(),
+description: Joi.string().optional(),
+campaignId: Joi.string().uuid().required(),
+};
 
-// export const createLocation: RequestHandler = async (req, res): Promise<void> => {
+export const createLocation: RequestHandler = async (req, res): Promise<void> => {
+    const { error, value } = Joi.object(LocationSchema).validate(req.body);
+    if (error) {
+        res.status(400).json({ error: "Invalid Location data" });
+        return;
+    }
+    // The campaign ID is hardcoded for now
+    const campaign = await CampaignService.getCampaign("1c932bb3-1bae-44aa-8536-c0aadf432882");
 
-// };
+    await CampaignService.createLocation(value as Location, campaign.id);
+
+    res.status(201).json("Location created successfully");
+};
 
 // export const getLocation: RequestHandler = async (req, res): Promise<void> => {
 
@@ -75,18 +85,28 @@ export const createNPC: RequestHandler = async (req, res): Promise<void> => {
 
 
 
-// // Item Section
+// Item Section
 
-// const ItemSchema = {
-//   id: Joi.string().uuid().optional(),
-//   name: Joi.string().required(),
-//   description: Joi.string().optional(),
-//   campaignId: Joi.string().uuid().required(),
-// };
+const ItemSchema = {
+  id: Joi.string().uuid().optional(),
+  name: Joi.string().required(),
+  description: Joi.string().optional(),
+  campaignId: Joi.string().uuid().required(),
+};
 
-// export const createItem: RequestHandler = async (req, res): Promise<void> => {
+export const createItem: RequestHandler = async (req, res): Promise<void> => {
+  const { error, value } = Joi.object(ItemSchema).validate(req.body);
+  if (error) {
+    res.status(400).json({ error: "Invalid Item data" });
+    return;
+  }
+  // The campaign ID is hardcoded for now
+  const campaign = await CampaignService.getCampaign("1c932bb3-1bae-44aa-8536-c0aadf432882");
 
-// };
+  await CampaignService.createItem(value as Item, campaign.id);
+
+  res.status(201).json("Item created successfully");
+};
 
 // export const getItem: RequestHandler = async (req, res): Promise<void> => {
 
@@ -102,18 +122,28 @@ export const createNPC: RequestHandler = async (req, res): Promise<void> => {
 
 
 
-// // PC Section
+// PC Section
 
-// const PCSchema = {
-//   id: Joi.string().uuid().optional(),
-//   name: Joi.string().required(),
-//   description: Joi.string().optional(),
-//   campaignId: Joi.string().uuid().required(),
-// };
+const PCSchema = {
+  id: Joi.string().uuid().optional(),
+  name: Joi.string().required(),
+  description: Joi.string().optional(),
+  campaignId: Joi.string().uuid().required(),
+};
 
-// export const createPC: RequestHandler = async (req, res): Promise<void> => {
+export const createPC: RequestHandler = async (req, res): Promise<void> => {
+  const { error, value } = Joi.object(PCSchema).validate(req.body);
+  if (error) {
+    res.status(400).json({ error: "Invalid Player Character data" });
+    return;
+  }
+  // The campaign ID is hardcoded for now
+  const campaign = await CampaignService.getCampaign("1c932bb3-1bae-44aa-8536-c0aadf432882");
 
-// };
+  await CampaignService.createPC(value as PC, campaign.id);
+
+  res.status(201).json("Player Character created successfully");
+};
 
 // export const getPC: RequestHandler = async (req, res): Promise<void> => {
 
