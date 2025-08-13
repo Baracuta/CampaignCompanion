caab9abf8e14525203ab9e6f2acb31209883426b
Benjamin Funk <lordbaracuta90@gmail.com>
2025-08-13 10:35:53 -0230

    Login-page-related commits

--- src/pages/SplashScreen.tsx
+++ src/pages/SplashScreen.tsx
@@ -2,21 +2,44 @@ import SplashBackground from "../components/SplashBackground";
 import "../css_modules/app.css"
 import NavButton from "../components/NavButton";
 import { ASSETS_PATH } from "../constants/assets_path";
+import { GoogleLogin } from "@react-oauth/google";
+import { useState } from "react";
 
 
 //This is the Splash Screen when the application starts.
 function SplashScreen() {
-  
+  const [logged, setLogged] = useState(false);
+
+  const handleLoginSuccess = () => { 
+    setLogged(true);
+  };
+  const handleLoginError = () => {
+    console.error("Login failed");
+  };
+
     return (
       <>
   
         <SplashBackground/>
   
-        <div className="splash_items">
+        <div className="splash_items" >
   
           <img className="logo" src={`${ASSETS_PATH}/Emblem 1 3.png`} alt="" />
           <h1>Campaign Companion</h1>
-          <div className="options">
+
+          <div style={{display: logged ? "none" : "flex"}}>
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                handleLoginSuccess();
+                console.log(credentialResponse);
+              }}
+              onError={handleLoginError}
+              theme="filled_black"
+              shape="circle"
+            />
+          </div>
+
+          <div className="options" style={{display: logged ? "flex" : "none"}}>
             <NavButton text="Create New Campaign" destination="/campaign-form"/>
             <NavButton text="Load Existing Campaign" destination="/campaign-select"/>
           </div>
