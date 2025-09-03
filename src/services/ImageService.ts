e9b21b70bae722386c223e144acbc8f0f14ebd9f
Benjamin Funk <lordbaracuta90@gmail.com>
2025-09-03 13:29:10 -0230

    Teeny-tiny commit that removes some ancient/obsolete comments from the ImageService in preparation to switch from an indexxedDB to SupaBase tomorrow.

--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -22,17 +22,14 @@ export async function clear() {
 export async function keys() {
   return (await dbPromise).getAllKeys("keyval");
 }
-// Need to make it so uploadImage checks to see if there is already a keyval pair for the thing.
-// OR, it needs to just delete the previous one if there is already one.
+
+
 export const uploadImage = async (img: string): Promise<string> => {
   const id = uuid();
   await set(id,img);
   return id
 };
 // Use this to make the above happen.
-// Prioritize making this work before next meeting
-// Look into refining the whole image upload process
-// Also have to get the favourite star clickable on the main thingdisplay
 export const getImage = async (imageId: string): Promise<string> => {
   const image = await get(imageId)
   return image
